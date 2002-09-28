d5a580dbd7c6f1c8016abfc911092fb47ad654e2
smurp <>
2002-09-28 01:01:23 +0000

    - checking in screwed up state

--- code/NooronApp.py
+++ code/NooronApp.py
@@ -1,6 +1,6 @@
 
-__version__='$Revision: 1.5 $'[11:-2]
-__cvs_id__ ='$Id: NooronApp.py,v 1.5 2002/08/23 03:39:15 smurp Exp $'
+__version__='$Revision: 1.6 $'[11:-2]
+__cvs_id__ ='$Id: NooronApp.py,v 1.6 2002/09/27 19:01:23 smurp Exp $'
 
 import GW
 from GWApp import GWApp
@@ -12,6 +12,7 @@ class NooronApp(GWApp):
     __allow_access_to_unprotected_subobjects__ = 1
     
     src_uris = []
+    app_skeleton = []
 
     def set_src_uris(self,src_uris):
         self.src_uris = src_uris
@@ -26,9 +27,12 @@ class NooronApp(GWApp):
             return None
 
 # see http://www.noosphere.org/discuss/zwiki/VariousTemplateUseCases
-    def publish(self,request,topic_obj):        
+    def publish(self,request,topic_obj):
+        npt_name = ""
         args = {}
+        this_is_app_front = 0
         if not topic_obj:
+            print "there is no topic_obj, determining ..."
             bname_contains = self.getAllWhereBaseNameContains
             bname_is = self.getAllWhereBaseNameIs
             app_instance = bname_contains('app instance') or \
@@ -36,20 +40,35 @@ class NooronApp(GWApp):
             if app_instance:
                 app_instances = app_instance[0].getInstances()
                 if app_instances:
+                    this_is_app_front = 1                    
                     topic_obj = app_instances[0]
         if not topic_obj:
             topic_obj = self
-        npt_name = self.get_template_for_topic(request,topic_obj)
+        if this_is_app_front:
+            npt_name = self.npt_for_app_front(request,topic_obj)
+        if not npt_name:
+            npt_name = self.get_template_for_topic(request,topic_obj)
         if __debug__: print "npt_name = ",npt_name
         if npt_name:
             request.effective_query_extend({'with_template':
                                             npt_name})
         NooronRoot.NooronRoot().publish(request,topic_obj)
 
+    def npt_for_app_front(self,request,topic_obj):
+        print "topic_obj = "+str(topic_obj)
+        occs = topic_obj.getOccurrences()#typ='npt for app front')
+        # relies on typ-sensitivity in getOccurrences
+        occs = None
+        if occs:
+            print "we got occs! " + str(len(occs))
+            print str(occs[0][2].getSCR().getUris()[0])
+            print str(occs[1][2].getSCR().getUris()[0])
+            return str(occs[0][2].getSCR().getUris()[0])
+
     def get_template_for_topic(self,request,topic_obj):
         query = request.split_query()
         return query.get('with_template') or \
-               self.npt_for_me(request,topic_obj) or \
+               self.npt_for_self(request,topic_obj) or \
                self.npt_for_instances(request,topic_obj)
 
     def npt_for_instances(self,request,topic_obj):
@@ -58,9 +77,16 @@ class NooronApp(GWApp):
             some = clss.getOccurrences(typ='npt_for_instances')
             if some:
                 return str(some[0][2].getSCR().getUris()[0])
+        print "no npt_for_instances found"
         
-    def npt_for_me(self,request,topic_obj):
-        some = topic_obj.getOccurrences(typ='npt_for_me')
+    def npt_for_self(self,request,topic_obj):
+        #some = topic_obj.getOccurrences()
+        #some = topic_obj.getOccurrences(typ='npt_for_self')
+        some = topic_obj.getOccurrences(typ='nooron_app_architecture.xtm#npt_for_self')
         if some:
+            #self.print_occurrenceResources(topic_obj)
+            for i in some:
+                print "--> " + str(i[2].getSCR().getData())
             return str(some[0][2].getSCR().getUris()[0])
+        print "failing to find npt_for_self"
 
