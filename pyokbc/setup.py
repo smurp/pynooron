a78189184f1ba22a1b99e30afe27565a22856055
smurp <>
2002-10-22 17:23:09 +0000

    - more release preparation

---
+++ pyokbc/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'pyokbc',
+    version = "0.1.0",
+    description = "An implementation of Open Knowledge Base Connectivity",
+    author = "Shawn F Murphy",
+    author_email = "smurp@emergence.com",
+    url = "http://www.noosphere.org/software/pyokbc",
+
+    packages = ['pyokbc'],
+    package_dir = {'pyokbc':'.'},
+    )
