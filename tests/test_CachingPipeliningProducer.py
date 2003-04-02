1d7b87ba1e5fe11cc31675c47307d2aef0b5a24d
smurp <>
2003-04-02 02:19:08 +0000

    testing the new file friendly techniques

--- tests/test_CachingPipeliningProducer.py
+++ tests/test_CachingPipeliningProducer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.1
 
-__version__='$Revision: 1.1 $'[11:-2]
-__cvs_id__ ='$Id: test_CachingPipeliningProducer.py,v 1.1 2003/02/07 22:53:16 smurp Exp $'
+__version__='$Revision: 1.2 $'[11:-2]
+__cvs_id__ ='$Id: test_CachingPipeliningProducer.py,v 1.2 2003/04/01 19:19:08 smurp Exp $'
 
 import os
 import sys
@@ -15,14 +15,13 @@ def str_sort(a,b):
 
 
 cache_path = '/tmp/nooron_cache'
+#cache_path = None
 
 canonical = "/know/somekb/someframe__dets.dot.ps.pdf\n" +\
             "kbdatestamp=1038939824\n" +\
             "nptdatestamp=1038932134\n"
 
-
-class dot_producer:
-    dot_eg = """digraph test123 {a -> b -> c -> a}"""
+class tedious_producer:
     def __init__(self):
         self._done = None
            
@@ -31,9 +30,58 @@ class dot_producer:
             return ''
         else:
             self._done = 1
-            return self.dot_eg
-           
+            return self.source
 
+class dot_producer(tedious_producer):
+    source = """digraph test123 {a -> b -> c -> a}"""
+
+class dbk_producer(tedious_producer):
+
+    a_pat = """
+         <sect2 id="%(name)s">
+            <title>%(title)s</title>
+	      <sect3 id="%(name)s-AKA">
+                <title>AKA</title>
+                   <para>%(AKA)s</para>                
+              </sect3>
+	      <sect3 id="%(name)s-Intent">
+                <title>Intent</title>
+                   <para>%(Intent)s</para>                
+              </sect3>
+	      <sect3 id="%(name)s-Motivation">
+                <title>Motivation</title>
+                   <para>%(Motivation)s</para>                
+              </sect3>
+         </sect2>              
+         """
+
+    pattern = {'name':'Visitor',
+               'title':'Visitorr',
+               'AKA':'Prowler',
+               'Intent':'Decouple a bunch of things.',
+               'Motivation':'A bunch of things are too coupleded.  ' * 516}
+    
+    source = """
+<!DOCTYPE article PUBLIC "-//OASIS//DTD DocBook V3.1//EN">
+ <article>    
+    <sect1 id="introduction">
+       <title>The Title Goes Here</title>
+        <para>The paragraph length introductory blurb goes here.</para>
+    </sect1>
+
+    <sect1 id="main-body">
+      <title>The Patterns</title>
+      %s
+    </sect1>
+ </article>
+    """ % a_pat % pattern
+
+
+       
+
+
+    
+    
 
 class CacheBehaviour(unittest.TestCase):
     def __init__(self,hunh):
@@ -43,6 +91,10 @@ class CacheBehaviour(unittest.TestCase):
     def empty_cache(self):
 
         cp = self.cp
+
+        if not cp._cachedir:
+            return
+        
         base = os.path.join(cp._cachedir,cp._cachekey)
 
         #proc = os.popen('ls '+base+'.*','r')
@@ -58,6 +110,9 @@ class CacheBehaviour(unittest.TestCase):
                 #print path
         
 
+
+
+
     def prepare_dot(self):
         cp = CachingPipeliningProducer(canonical_request=canonical)
 
@@ -72,6 +127,10 @@ class CacheBehaviour(unittest.TestCase):
     def prepare_pdf(self):
         self.prepare_dot()
         cp = self.cp
+        cp.append_pipe(PipeSection(producer=dot_producer(),
+                                   extension='dot',
+                                   mimetype ='application/x-graphviz'))
+
         cp.append_pipe(PipeSection(command='dot -Tps ',
                                    extension='ps',
                                    mimetype = 'application/ps'))
@@ -82,15 +141,60 @@ class CacheBehaviour(unittest.TestCase):
 
 
 
+    def prepare_dbk(self):
+        cp = CachingPipeliningProducer(canonical_request=canonical)
+
+        cp.append_pipe(PipeSection(producer=dbk_producer(),
+                                   extension='dbk',
+                                   mimetype ='text/xml'))
+
+        cp.append_pipe(PipeSection(extension='ps',
+                                   readsfrom='file',
+                                   writesto='file',
+                                   command='docbook2ps --output %(cache_dir)s %(precursor)s',
+                                   mimetype ='application/postscript'))
+
+##        cp.append_pipe(PipeSection(command='sort ',
+##                                   extension='sorted',
+##                                   mimetype = 'application/sorted'))
+
+##        cp.append_pipe(PipeSection(command='uniq ',
+##                                   extension='uniq',
+##                                   mimetype = 'text/plain'))
+
+        cp.set_cachedir(cache_path)
+
+        self.cp = cp
+
+
+    def test_cacheless(self):
+        self.prepare_dbk()
+        #self.cp.set_cachedir(None)
+        print "got to here"
+        prime_type = self.cp.prime()
+        print prime_type
+        out = self.cp.more()
+        cl = self.cp.content_length()
+        print "content length =",cl
+        print "=================================="
+
+    def stest_dbk(self):
+        self.prepare_dbk()
+        self.empty_cache()
+        self.assertEquals('text/xml', self.cp.mimetype())
+        prime_type = self.cp.prime()
+        print self.cp.more()
+
     def test_dot(self):
         self.prepare_dot()
         self.empty_cache()
 
-        self.assertEquals('application/x-graphviz', self.cp.mimetype())        
+        self.assertEquals('application/x-graphviz', self.cp.mimetype())
         prime_type = self.cp.prime()
         out = self.cp.more()
         self.assertEquals('freshly-generated',prime_type)        
         cl = self.cp.content_length()
+
         actual_length = len(out)
         self.assertEquals(actual_length,cl)
 
@@ -122,7 +226,7 @@ class CacheBehaviour(unittest.TestCase):
         self.assertEquals('from-cache',prime_type)
 
 
-
+    
 
 
 if __name__ == "__main__":
