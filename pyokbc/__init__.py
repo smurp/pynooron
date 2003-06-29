1bece6fc6db45228ad3bd14468119218d96ffb59
smurp <>
2003-06-29 03:48:42 +0000

    added WikiWord and cvs Id

--- pyokbc/__init__.py
+++ pyokbc/__init__.py
@@ -1,3 +1,8 @@
+
+__version__='$Revision: 1.7 $'[11:-2]
+__cvs_id__ ='$Id: __init__.py,v 1.7 2003/06/28 21:48:42 smurp Exp $'
+
 from PyOkbc import *
 from FileSystemConnection import FileSystemConnection
 from OkbcConditions import *
+from WikiWord import *
